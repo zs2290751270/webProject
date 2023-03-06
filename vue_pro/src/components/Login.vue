35649490c54bbe4af2724f72570b89a0d590fe90
王迪 <848174306@qq.com>
2023-03-06 18:13:04 +0800

    ✨ 登录

---
+++ vue_pro/src/components/Login.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="login-container">
+    <el-form
+      ref="form"
+      :model="loginForm"
+      :rules="rules"
+      label-position="left"
+      label-width="80px"
+    >
+      <el-form-item label="用户名" prop="username">
+        <el-input
+          v-model="loginForm.username"
+          placeholder="请输入用户名"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input
+          v-model="loginForm.password"
+          placeholder="请输入密码"
+          type="password"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import axios from '../api'
+import { ElMessage } from 'element-plus'
+
+export default defineComponent({
+  name: 'LoginPage',
+  setup () {
+    const loginForm = ref({
+      username: '',
+      password: ''
+    })
+
+    const rules = {
+      username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+      password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+    }
+
+    const form = ref(null)
+
+    const submitForm = () => {
+      form.value.validate(valid => {
+        if (valid) {
+          axios.post('/api/login/', loginForm.value).then(res => {
+            const { access, refresh } = res
+            console.log('🚀 ~ access, refresh:', access, refresh)
+            ElMessage({
+              message: '登陆成功',
+              type: 'success'
+            })
+          })
+        } else {
+          return false
+        }
+      })
+    }
+
+    return {
+      loginForm,
+      rules,
+      submitForm,
+      form
+    }
+  }
+})
+</script>
+
+<style>
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
--- vue_pro/src/main.js
+++ vue_pro/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// 引入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount('#app')
